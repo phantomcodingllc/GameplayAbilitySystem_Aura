b28b014d60214ee80e05b5823df060465563b449
Darrell <67837432+phantomcodingllc@users.noreply.github.com>
2024-09-20 19:33:44 -0400

    Added Movement Input to Player Character

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -3,11 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
 
 class UInputMappingContext;
+class UInputAction;
+struct F1InputActionValue;
 /**
  * 
  */
@@ -20,8 +23,14 @@ class AURA_API AAuraPlayerController : public APlayerController
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 private:
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
